400735df3c2caeb40da5fb1e789d481aaedfac09
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-04-20 00:55:59 -0300

    change get to post

--- routes/api/setupRoutes.js
+++ routes/api/setupRoutes.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var setupController = require('../../controllers/api/setupController')
 
-router.get('/', setupController.setup)
+router.post('/', setupController.setup)
 
 module.exports = router;
