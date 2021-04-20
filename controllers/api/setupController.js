b3020b332cc76455dcf52835fe41069554ec3756
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-04-20 00:14:17 -0300

    add controller to do setup if it was not done

---
+++ controllers/api/setupController.js
@@ -0,0 +1,26 @@
+const setupService = require("../../services/setupService");
+const db = require("../../database/models");
+
+const setupController = {
+
+    setup: async function (req, res) {
+
+        db.sequelize.query('show tables')
+            .then(data => {
+                setupService.setup();
+
+                const rowsQuantity = data[0].length;
+                if (rowsQuantity) {
+                    return res.status(200).json({ setup: 'La base de datos tiene tablas' });
+                }
+                else {
+                    setupService.setup();
+                    return res.status(200).json({ setup: 'La base de datos fue creada y populada' });
+                }
+
+            });
+
+    }
+}
+
+module.exports = setupController;
