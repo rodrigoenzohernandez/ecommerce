75dd8085212f16c53b0f273e4ee8ea0b6f18c61c
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-04-20 00:14:31 -0300

    delete files

--- services/InfoService.js
+++
@@ -1,9 +0,0 @@
-const db = require("../database/models");
-var _ = require('lodash');
-
-module.exports = {
-    setupDone: async function () {
-        const { rows } = (await db.sequelize.query(`SHOW TABLES`));
-        return await Promise.resolve(!_.isEmpty(rows));
-    }
-}
